716090a9d52c29ba69b0db51f970b778405c6576
dconco <onipededavid208@gmail.com>
2025-05-15 03:20:03 +0100

    Initialize project with CMake configuration and basic main.cpp structure

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+   std::cout << "Hello B!" << std::endl;
+   // return 0;
+   _sleep(5000); // Sleep for 5 seconds
+   std::cout << "Hello C!" << std::endl;
+   return 0;
+}
